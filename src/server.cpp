b93b1fb795ed8bdddc3bac89f5899d9016e98c00
Aayush Acharya <aayushacharya007@gmail.com>
2025-06-15 03:11:54 -0500

    test

--- src/server.cpp
+++ src/server.cpp
@@ -183,7 +183,10 @@ void handleClient(int client_fd) {
         }
         if (hasGzip) {
           std::string body = gzipCompress(contentStr);
-          response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: " +  std::to_string(body.size()) + "\r\n\r\n" + body.data();
+          response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: " +  std::to_string(body.size()) + "\r\n\r\n";
+          send(client_fd, response.c_str(), strlen(response.c_str()), 0);
+          send(client_fd, body.data(), body.size(), 0);
+          return;
         }
         else {
           response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
